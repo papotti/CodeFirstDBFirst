8225f680e7ce1401b21fdbc51e7db180be6b1f03
Marco Papotti <papotti.marco@outlook.it>
2023-02-16 10:34:45 +0100

    Aggiungere i file di progetto.

---
+++ CodeFirstDBFirst/DB/Tabella1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirstDBFirst.DB;
+
+public partial class Tabella1
+{
+    public int Pk { get; set; }
+
+    public string? Descr { get; set; }
+}
---
+++ CodeFirstDBFirst/DB/TestContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstDBFirst.DB;
+
+public partial class TestContext : DbContext
+{
+    public TestContext()
+    {
+    }
+
+    public TestContext(DbContextOptions<TestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tabella1> Tabella1 { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=MSI;Database=provaCFDF;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tabella1>(entity =>
+        {
+            entity.HasKey(e => e.Pk);
+
+            entity.Property(e => e.Pk)
+                .ValueGeneratedNever()
+                .HasColumnName("PK");
+            entity.Property(e => e.Descr)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("DESCR");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
